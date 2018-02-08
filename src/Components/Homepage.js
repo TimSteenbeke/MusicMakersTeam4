c879758ca0cea8a9d6283d976456bf9b40e9728e
Jari Van Meirvenne <jarivmeirvenne@gmail.com>
2018-02-08 15:27:01 +0100

    Homepage + gitignore file

---
+++ src/Components/Homepage.js
@@ -0,0 +1,25 @@
+/**
+ * Created by jariv on 8/02/2018.
+ */
+
+import React, {Component} from 'react';
+import '../CSS/Homepage.css';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+class HomepageComponent extends Component {
+    render() {
+        return (
+            <div className="Homepage">
+                <section className="container">
+                    <div className="left-half">
+
+                    </div>
+                    <div className="right-half">
+
+                    </div>
+                </section>
+            </div>
+        );
+    }
+}
