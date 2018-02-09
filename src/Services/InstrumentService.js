5676f2a5e32d92cdffdab7227dee8c4c83b08fd3
LorenzLambot <lorenz.lambot@gmail.com>
2018-02-09 13:43:20 +0100

    instrumenten service

---
+++ src/Services/InstrumentService.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Lo on 4/08/2017.
+ */
+const BASE_PATH = "backend/";
+
+
+export function getInstrumentenFromBackend() {
+    return fetch("http://musicmaker-api-team4.herokuapp.com/api/instruments")
+        .then((response) => response.json())
+        .then((responseJson) => {
+            return responseJson.data;
+        })
+        .catch((err) => {
+            console.log("geen response");
+        });
+}
+
+export function getInstrumentFromBackend(instrumentNr) {
+    return fetch("http://musicmaker-api-team4.herokuapp.com/api/instruments")
+        .then((response) => response.json())
+        .then((responseJson) => {
+            return responseJson.data;
+        })
+        .catch((err) => {
+            const instrument = {naam:"instrumenten niet gevonden"};
+            return instrument;
+        });
+}
