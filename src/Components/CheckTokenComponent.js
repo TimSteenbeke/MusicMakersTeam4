2b69cadc4fe8119c64edb0afcd68e179f38ef994
Jari Van Meirvenne <jarivmeirvenne@gmail.com>
2018-03-16 21:28:00 +0100

    Fixes for all

--- src/Components/CheckTokenComponent.js
+++ src/Components/CheckTokenComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import * as LoginService from "../Services/LoginService";
-import Login from './Login.js';
+import Home from './Home.js';
 /**
  * Higher-order component (HOC) to wrap restricted pages
  */
@@ -23,7 +23,7 @@ export default function(WrapperComponent) {
             if(this.checkAuthentication(this.props)){
                 return <WrapperComponent {...this.props} />
             }else{
-                return <Login/>
+                return <Home/>
             }
         }
     }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,8 +5,7 @@
 import guitar from '../images/guitar.jpg';
 import React, {Component} from 'react';
 import Header from './Header';
-import Divider from 'material-ui/Divider';
-import {Card,CardTitle} from 'react-materialize';
+import * as LoginService from "../Services/LoginService";
 
 
 export default class Home extends Component{
@@ -24,41 +23,57 @@ export default class Home extends Component{
 
 
     render(){
-        return (
-        <div className="Homepage">
-            <Header name="Home"/>
-            <div className="row">
-                <div className="col s12 m10 offset-m1 l10 offset-l1">
-                    <div className="card hoverable z-depth-3">
-                        <div className="card-image waves-effect waves-block waves-light">
-                            <img className="activator" alt="guitar" src={guitar} />
+        {
+            if (LoginService.checkToken()){
+            return (
+                <div className="Homepage">
+                    <Header name="Home"/>
+                    <div className="row">
+                        <div className="col s12 m10 offset-m1 l10 offset-l1">
+                            <div className="card hoverable z-depth-3">
+                                <div className="card-image waves-effect waves-block waves-light">
+                                    <img className="activator" alt="guitar" src={guitar}/>
+                                </div>
+                                <div className="card-content darken-2">
+                                    <span className="card-title activator black-text text-darken-4">Melding<i
+                                        className="material-icons right">more_vert</i></span>
+                                </div>
+                                <div className="card-reveal darken-2 lighten-4 black-text">
+                                    <span className="card-title black-text text-darken-4">Melding<i
+                                        className="material-icons right">close</i></span>
+                                    <p className="black-text">Yes boi</p>
+                                </div>
+                            </div>
                         </div>
-                        <div className="card-content darken-2">
-                            <span className="card-title activator black-text text-darken-4">Melding<i className="material-icons right">more_vert</i></span>
-                        </div>
-                        <div className="card-reveal darken-2 lighten-4 black-text">
-                            <span className="card-title black-text text-darken-4">Melding<i className="material-icons right">close</i></span>
-                            <p className="black-text">Yes boi</p>
-                        </div>
-                    </div>
-                </div>
 
-                <div className="col s12 m10 offset-m1 l10 offset-l1">
-                    <div className="card hoverable z-depth-3">
-                        <div className="card-image waves-effect waves-block waves-light">
-                            <img className="activator" alt="guitar" src={guitar} />
-                        </div>
-                        <div className="card-content darken-2">
-                            <span className="card-title activator black-text text-darken-4">Melding<i className="material-icons right">more_vert</i></span>
-                        </div>
-                        <div className="card-reveal darken-2 lighten-4 black-text">
-                            <span className="card-title black-text text-darken-4">Melding<i className="material-icons right">close</i></span>
-                            <p className="black-text">Yes boi</p>
+                        <div className="col s12 m10 offset-m1 l10 offset-l1">
+                            <div className="card hoverable z-depth-3">
+                                <div className="card-image waves-effect waves-block waves-light">
+                                    <img className="activator" alt="guitar" src={guitar}/>
+                                </div>
+                                <div className="card-content darken-2">
+                                    <span className="card-title activator black-text text-darken-4">Melding<i
+                                        className="material-icons right">more_vert</i></span>
+                                </div>
+                                <div className="card-reveal darken-2 lighten-4 black-text">
+                                    <span className="card-title black-text text-darken-4">Melding<i
+                                        className="material-icons right">close</i></span>
+                                    <p className="black-text">Yes boi</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        )
+            )}
+            else{
+                return (
+                    <div>
+                        <Header name="Please login"/>
+                        <h4 className="center-align">Deze app is voor leden van Music Makers</h4>
+                    </div>
+                )
+            }
+
+        }
     }
 }
