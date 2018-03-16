bf3b8ff0b59ebec6d09c6bd5968429da7895f91f
TimSteenbeke <tim.steenbeke@student.kdg.be>
2018-03-16 15:07:11 +0100

    Username - firstname

--- src/Components/ChatComponents/ChatComponent.js
+++ src/Components/ChatComponents/ChatComponent.js
@@ -1,8 +1,9 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import * as Stomp from 'stompjs';
 import * as SockJS from 'sockjs-client';
 import {TextField} from "material-ui";
 import {black500, deepOrangeA700, grey500} from "material-ui/styles/colors";
+
 const styles = {
     width: {
         width: "90%",
@@ -29,8 +30,9 @@ const styles = {
 };
 
 
-
+// const serverUrl = 'https://musicmaker-api-team4.herokuapp.com/socket';
 const serverUrl = 'http://localhost:8080/socket';
+const currentUser = JSON.parse(localStorage.getItem('currentUser'));
 
 export default class ChatComponent extends Component {
     constructor(props) {
@@ -38,10 +40,10 @@ export default class ChatComponent extends Component {
         this.state = {
             title: 'WebSockets chat',
             text: "",
-            chatroom: 'poef',
-            messages: ["10:20:50 - Tim: Hello there bro","10:20:55 - Jos: jo test"],
-            message: "Hello there, Users",
-            name: "UserX"
+            chatroom: 'Poef',
+            messages: ["Welcome to chatroom Poef", "Tim: Hello there", "Jos: jo test"],
+            message: "Hello world !",
+            name: currentUser.firstname
         };
         this.stompClient = null;
     }
@@ -51,6 +53,10 @@ export default class ChatComponent extends Component {
     }
 
     ChangeRoom() {
+        let self = this;
+        self.setState({
+            messages: ["Welcome to Chatroom " + self.state.chatroom]
+        });
         this.initializeWebSocketConnection();
     }
 
@@ -60,7 +66,7 @@ export default class ChatComponent extends Component {
         self.stompClient = Stomp.over(ws);
         console.log("initializeWebSocketConnection");
         console.log(ws);
-        self.stompClient.connect({},() => {
+        self.stompClient.connect({}, () => {
             self.stompClient.subscribe('/chat/' + self.state.chatroom, (message) => {
                 console.log("msg");
                 console.log(message);
@@ -101,7 +107,7 @@ export default class ChatComponent extends Component {
                     value={this.state.message}
                 />
                 <button onClick={(e) => this.sendMessage(e)}>send</button>
-                <label>Naam:</label>
+                {/*<label>Naam:</label>
                 <TextField
                     style={styles.width}
                     hintText="Type your name here..."
@@ -115,7 +121,7 @@ export default class ChatComponent extends Component {
                         this.setState({name: typedName});
                     }}
                     value={this.state.name}
-                />
+                />*/}
                 <label>Chatroom:</label>
                 <TextField
                     style={styles.width}
@@ -132,9 +138,11 @@ export default class ChatComponent extends Component {
                     value={this.state.chatroom}
                 />
                 <button onClick={(e) => this.ChangeRoom(e)}>Change room</button>
-
+                <div>
+                    <p>|</p>
+                </div>
                 <div className="chat">
-                    {this.state.messages.map((msg,key) => {
+                    {this.state.messages.map((msg, key) => {
                             return (<div id={key}>{msg}</div>)
                         }
                     )}
