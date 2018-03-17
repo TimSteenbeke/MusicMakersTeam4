e0c567463439aafb7062ff80456e411bc16789cd
vanraemdonckben <ben.vanraemdonck@student.kdg.be>
2018-03-17 14:44:11 +0100

    Update show initial info
    TODO: fix PUT request

--- src/Components/CoursesDetails.js
+++ src/Components/CoursesDetails.js
@@ -10,7 +10,10 @@ import * as CourseService from '../Services/CourseService'
 import Header from './Header'
 import StyledTextField from './StyledTextField'
 import {Link} from 'react-router-dom';
+import {Row, Input} from 'react-materialize'
 import swal from 'sweetalert2'
+import * as CourseTypeService from "../Services/CourseTypeService";
+import * as UserService from "../Services/UserService";
 
 class CoursesDetails extends Component {
 
@@ -18,7 +21,13 @@ class CoursesDetails extends Component {
         super(props);
         this.state = {
             courseId: this.props.match.params.id,
-            beschrijving: "string",
+            courseTypes: [],
+            teachers : [],
+            students: [],
+
+            selectedTeachers :[],
+            selectedStudents :[],
+            selectedCourseType: 0
         }
 
     }
@@ -31,13 +40,24 @@ class CoursesDetails extends Component {
             .then(course => {
                 self.setState({
                     courseId: this.props.match.params.id,
-                    beschrijving: course.description,
+                    selectedCourseType: course.courseType.courseTypeId,
+                    selectedTeachers:  course.teachers.map( teacher => teacher.id),
+                    selectedStudents: course.students.map( student => student.id),
+
+
+
                 });
+                console.log(course);
                 console.log(self.state.beschrijving);
                 console.log(course.users);
+                console.log(this.state.selectedStudents);
             }).catch((error) => {
             console.log(error);
         });
+
+        this.addCourseTypes();
+        this.addStudents();
+        this.addTeachers();
     }
 
     handleUpdate = () => {
@@ -60,38 +80,124 @@ class CoursesDetails extends Component {
     };
 
 
-    onChangeDescription = (e) => {
-        this.setState({beschrijving: e.target.value});
-        console.log("beschrijving:" + e.target.value)
+
+    addCourseTypes = () => {
+        CourseTypeService.getCourseTypesFromBackend().then(console.log("-----CourseTypes------"))
+            .then(courseTypes => {
+                this.setState({courseTypes: courseTypes});
+                console.log(courseTypes);
+            })
+    }
+
+    addStudents = () => {
+        UserService.getStudents().then(console.log("----students---- \n"))
+            .then(students => {
+                this.setState({students: students.users}, console.log(students.users));
+                console.log(students);
+            });
+        console.log("this state");
+        console.log(this.state.students);
     };
+
+    addTeachers = () => {
+        UserService.getTeachers().then(console.log("----teachers---- \n"))
+            .then(teachers => {
+                this.setState({teachers: teachers.users}, console.log(teachers.users));
+                console.log('teachers');
+                console.log(teachers);
+            });
+
+    };
+
+
+
 
     render() {
         return (
             <div className="Homepage">
-                <Header name={"Course " + this.state.courseId}/>
-
+                <Header name="Update Course"/>
                 <section className="containerCss">
                     <div className="row">
                         <div className="col s0 m2 l2"/>
                         <div className="col s12 m8 l8">
-                            <div className="card hoverable z-depth-3">
+                            <div className="card hoverable">
                                 <div className="card-content">
-                                    <div className="section">
-                                        <div className="row">
-                                            <div className="col s3 m3 l3">
-                                                <h5 className="truncate">{this.state.beschrijving}</h5>
-                                            </div>
-                                            <div className="col s9 m9 l9">
-                                                <StyledTextField onChange={this.onChangeDescription} hint="Geef nieuw coursebeschrijving in..."
-                                                                 label="Coursebeschrijving"/>
+                                    <form className="addCourse" action="/" method="POST" onSubmit={(e) => {
+                                        e.preventDefault();
+                                        this.handleClick();
+                                    } }>
+                                        <div className="section">
+                                            <div className="row">
+                                                <div className="col s3 m3 l3">
+                                                    <h5 className="truncate">Beschrijving</h5>
+                                                </div>
+                                                <div className="col s9 m9 l9">
+                                                    <Row>
+                                                        <Input s={12} multiple={false} type='select'
+                                                               onChange={this.handleCourseTypeChange}
+                                                               label="Leerkrachten" icon='face' value={this.state.selectedCourseType}>
+                                                            <option key="" value="" disabled>Kies lesType
+                                                            </option>
+                                                            {this.state.courseTypes.map((courseType, index) => (
+                                                                <option key={courseType.courseTypeId}
+                                                                        value={courseType.courseTypeId}>{courseType.description}</option>
+                                                            ))}
+                                                        </Input>
+                                                    </Row>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
+                                        <div className="divider"></div>
+                                        <div className="section">
+                                            <div className="row">
+                                                <div className="col s3 m3 l3">
+                                                    <h5 className="truncate">Leerkrachten</h5>
+                                                </div>
+                                                <div className="col s9 m9 l9">
+                                                    <Row>
+                                                        <Input s={12} multiple={true} type='select'
+                                                               onChange={this.handleTeacherChange}
+                                                               label="Leerkrachten" icon='face' value={this.state.selectedTeachers}>
+                                                            <option key="" value="" disabled>Kies de leerkrachten
+                                                            </option>
+                                                            {this.state.teachers.map((teacher, index) => (
+                                                                <option key={teacher.userid}
+                                                                        value={teacher.userid}>{teacher.firstname} {teacher.lastname}</option>
+                                                            ))}
+                                                        </Input>
+                                                    </Row>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="section">
+                                            <div className="row">
+                                                <div className="col s3 m3 l3">
+                                                    <h5 className="truncate">Studenten</h5>
+                                                </div>
+                                                <div className="col s9 m9 l9">
+                                                    <Row>
+                                                        <Input s={12} multiple={true} type='select' label="Studenten" onChange={this.handleStudentChange}
+                                                               icon='child_care' value={this.state.selectedStudents}>
+                                                            <option key="" value="" disabled>Kies de studenten</option>
+                                                            {this.state.students.map((student, index) => (
+                                                                <option key={student.userid}
+                                                                        value={student.userid}>{student.firstname} {student.lastname}</option>
+                                                            ))}
+                                                        </Input>
+                                                    </Row>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="divider"></div>
+                                    </form>
+
                                 </div>
+
                                 <div className="card-action">
-                                    <Link to="/courses" onClick={this.handleUpdate}
-                                          className="btn-floating btn-small waves-effect waves-light deep-orange darken-4 pulse"><i
-                                        className="material-icons">done</i>
+                                    <Link to="/courses" onClick={this.handleClick}
+                                          className="btn-floating btn-small waves-effect waves-light deep-orange darken-4 pulse">
+                                        <i
+                                            className="material-icons">done</i>
                                     </Link>
                                 </div>
                             </div>
