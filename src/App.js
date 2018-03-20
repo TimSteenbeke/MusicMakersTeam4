6a32a438c5b7aa9221277ecce2b4e2c9946dc34d
TimSteenbeke <tim.steenbeke@student.kdg.be>
2018-03-20 20:05:47 +0100

    redirect wrong pages to home

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import PlayMusic from './Components/MusicComponents/PlayMusic.js';
 import NotFound from "./Components/GeneralComponents/NotFound";
 import CompositionUpdate from "./Components/CompositionComponents/CompositionUpdate";
 import auth from './Components/GeneralComponents/CheckTokenComponent';
+import role from './Components/GeneralComponents/CheckRoleComponent';
 import AddCourseType from "./Components/CourseComponents/AddCourseType";
 import CourseTypeDetails from "./Components/CourseComponents/CourseTypeDetails";
 import CourseTypes from "./Components/CourseComponents/CourseTypes";
@@ -40,23 +41,23 @@ export default class App extends Component {
         return (
             <Switch>
                 <Route name="home" exact path="/" component={Home}/>
-                <Route name="addInstrument" path="/addinstrument" component={auth(AddInstrument)}/>
-                <Route name="instrument" path="/instrumenten" component={auth(Instrumenten)}/>
-                <Route name="instrumentDetails" path="/instrumentdetails/:id" component={auth(InstrumentDetails)}/>
+                <Route name="addInstrument" path="/addinstrument" component={auth(role(AddInstrument))}/>
+                <Route name="instrument" path="/instrumenten" component={auth(role(Instrumenten))}/>
+                <Route name="instrumentDetails" path="/instrumentdetails/:id" component={auth(role(InstrumentDetails))}/>
 
-                <Route name="courseDetails" path="/coursedetails/:id" component={auth(CourseDetails)}/>
-                <Route name="courses" path="/courses" component={auth(Courses)}/>
-                <Route name="addCourse" path="/addcourse" component={auth(AddCourse)}/>
+                <Route name="courseDetails" path="/coursedetails/:id" component={auth(role(CourseDetails))}/>
+                <Route name="courses" path="/courses" component={auth(role(Courses))}/>
+                <Route name="addCourse" path="/addcourse" component={auth(role(AddCourse))}/>
 
                 <Route name="agenda" path="/agenda" component={auth(Agenda)}/>
 
                 <Route name="compositions" exact path="/compositions" component={auth(Compositions)}/>
-                <Route name="addComposition" path="/addcomposition" component={auth(AddComposition)}/>
-                <Route name="compositionDetails" path="/compositions/:id" component={auth(CompositionUpdate)}/>
+                <Route name="addComposition" path="/addcomposition" component={auth(role(AddComposition))}/>
+                <Route name="compositionDetails" path="/compositions/:id" component={auth(role(CompositionUpdate))}/>
 
-                <Route name="groups" path="/groups" component={auth(Group)}/>
-                <Route name="addGroup" path="/addgroup" component={AddGroup}/>
-                <Route name="groupUpdate" path="/groupupdate/:id" component={GroupUpdate}/>
+                <Route name="groups" path="/groups" component={auth(role(Group))}/>
+                <Route name="addGroup" path="/addgroup" component={auth(role(AddGroup))}/>
+                <Route name="groupUpdate" path="/groupupdate/:id" component={auth(role(GroupUpdate))}/>
 
                 <Route name="myGroups" path="/mygroups" component={auth(MyGroup)}/>
                 <Route name="myGroupDetails" path="/mygroupdetails/:id" component={auth(MyGroupDetails)}/>
@@ -66,20 +67,20 @@ export default class App extends Component {
 
                 <Route name="play" path="/play/:id" component={auth(PlayMusic)}/>
 
-                <Route name="addCourseTypes" path="/addcoursetype" component={auth(AddCourseType)}/>
-                <Route name="courseTypes" path="/coursetypes" component={auth(CourseTypes)}/>
-                <Route name="courseTypeDetails" path="/coursetypedetails/:id" component={auth(CourseTypeDetails)}/>
-                <Route name="addLesson" path="/addLesson" component={auth(AddLesson)}  />
+                <Route name="addCourseTypes" path="/addcoursetype" component={auth(role(AddCourseType))}/>
+                <Route name="courseTypes" path="/coursetypes" component={auth(role(CourseTypes))}/>
+                <Route name="courseTypeDetails" path="/coursetypedetails/:id" component={auth(role(CourseTypeDetails))}/>
+                <Route name="addLesson" path="/addLesson" component={auth(role(AddLesson))}  />
 
-                <Route name="users" path="/users" component={auth(Users)}/>
-                <Route name="addUser" path="/addUser" component={auth(AddUser)}/>
-                <Route name="userDetails" path="/userdetails/:id" component={auth(UserDetails)}/>
+                <Route name="users" path="/users" component={auth(role(Users))}/>
+                <Route name="addUser" path="/addUser" component={auth(role(AddUser))}/>
+                <Route name="userDetails" path="/userdetails/:id" component={auth(role(UserDetails))}/>
 
                 <Route name="chat" path="/chat" component={auth(ChatComponent)}/>
 
                 <Route name="newsitems" path="/newsitems" component={auth(NewsItems)}/>
-                <Route name="addNewsItem" path="/addNewsItem" component={auth(AddNewsItem)}/>
-                    <Route name="updatenNewsItem" path="/updatenewsitem/:id" component={auth(UpdateNewsItem)}/>
+                <Route name="addNewsItem" path="/addNewsItem" component={auth(role(AddNewsItem))}/>
+                    <Route name="updatenNewsItem" path="/updatenewsitem/:id" component={auth(role(UpdateNewsItem))}/>
 
 
                     <Route path='*' exact={true} component={NotFound}/>
---
+++ src/Components/GeneralComponents/CheckRoleComponent.js
@@ -0,0 +1,39 @@
+import * as userService from "../../Services/UserService";
+import React, {Component} from "react";
+import Home from '../Home.js';
+
+export default function (WrapperComponent) {
+    class CheckRoleComponent extends Component {
+
+        CheckUserRoles(params) {
+            const {history} = params;
+            let roles = [];
+            if (localStorage.getItem("userToken") != null) {
+                userService.getRolesCurrentUser().then(
+                    (value) => {
+                        roles = value.roles;
+                        if(roles[0].roleid <3){
+                            return true;
+                        }
+/*                        roles.forEach(role => {
+                            if(role.roleid<3){
+                            // if (role.roleid === 1 || role.roleid === 2) {
+                                return true;
+                            }
+                        });*/
+                    });
+            }
+            return false;
+        }
+
+        render() {
+            if (this.CheckUserRoles(this.props)) {
+                return <WrapperComponent {...this.props} />
+            } else {
+                return <Home/>
+            }
+        }
+    }
+
+    return CheckRoleComponent;
+}
