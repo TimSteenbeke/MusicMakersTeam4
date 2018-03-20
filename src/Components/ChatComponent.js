3029149338eecf5df97e658d1c11abc5dcd49e76
Jari Van Meirvenne <jarivmeirvenne@gmail.com>
2018-03-20 01:59:27 +0100

    Chat is af boyz

--- src/Components/ChatComponent.js
+++ src/Components/ChatComponent.js
@@ -4,11 +4,13 @@ import * as SockJS from 'sockjs-client';
 import StyledTextField from './GeneralComponents/StyledTextField';
 import Header from './GeneralComponents/Header';
 import './ChatComponent.css';
-
-
+import {List, ListItem} from 'material-ui/List';
+import * as GroupService from '../Services/GroupService.js';
+import * as UserService from '../Services/UserService';
 const serverUrl = 'https://musicmaker-api-team4.herokuapp.com/socket';
 // const serverUrl = 'http://localhost:8080/socket';
 const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+
 
 export default class ChatComponent extends Component {
     constructor(props) {
@@ -16,30 +18,47 @@ export default class ChatComponent extends Component {
         this.state = {
             title: "WebSockets chat",
             text: "",
-            chatroom: "Poef",
-            currentChatroom: "Poef",
+            currentChatroom: "Groep 1",
             messages: [],
-            message: "Hello world !",
-            name: currentUser
+            message: {
+                userName: "",
+                userId: 1,
+                text: ""
+            },
+            user: {},
+            groups: []
         };
         this.stompClient = null;
         console.log("user " + this.state.name);
     }
 
     componentDidMount() {
-        this.initializeWebSocketConnection();
-    }
-
-    ChangeRoom = () => {
         let self = this;
-        console.log(self.state.chatroom);
-        console.log(self.state.currentChatroom);
-        this.setState({currentChatroom: self.state.chatroom, messages: []}, () => {
-            console.log("chat =>" + self.state.currentChatroom);
-            self.initializeWebSocketConnection();
+        UserService.getUserByUsernameFromBackend().then(user => {
+            self.setState({user: user}, () => {
+                self.setState({message: {userId: user.id}});
+                self.setState({message: {userName: user.firstname + ' ' + user.lastname}});
+            });
+            console.log("Users \n \n => " + this.state.user.firstname);
+            console.log("Users Message \n \n => " + this.state.message.userId);
+            GroupService.getGroupsByUser().then(groups => {
+                    self.setState({groups: groups, currentChatroom: groups[0].name});
+                }
+            ).then(()=> {
+                self.initializeWebSocketConnection();
+            });
         });
 
 
+    }
+
+    ChangeRoom = (name) => {
+        let self = this;
+        console.log(self.state.currentChatroom);
+        this.setState({currentChatroom: name, messages: []}, () => {
+            console.log("chat =>" + self.state.currentChatroom);
+            self.initializeWebSocketConnection();
+        });
     };
 
     initializeWebSocketConnection = () => {
@@ -49,7 +68,7 @@ export default class ChatComponent extends Component {
         console.log("initializeWebSocketConnection");
         console.log(ws);
         console.log("OH BOI HERE WE GO :" + self.state.currentChatroom);
-
+        console.log("WEEEEEEEEEE :" + self.state.message.userId);
         self.stompClient.connect({}, () => {
             self.stompClient.subscribe('/chat/' + self.state.currentChatroom, (message) => {
                 console.log("msg");
@@ -57,76 +76,74 @@ export default class ChatComponent extends Component {
                 console.log("currentChatRoom");
                 console.log(this.state.currentChatroom);
                 if (message.body) {
-                    self.setState({messages: [...self.state.messages, message.body]});
-                    console.log(message.body);
+                    self.setState({messages: [...self.state.messages, JSON.parse(message.body)]});
+                    console.log("BOWDY => " + JSON.parse(message.body).userId);
                 }
             });
         });
         console.log("init is done");
     };
 
-    sendMessage() {
-        let name = this.state.name;
-        let content = this.state.message;
-        let message = name + ': ' + content;
-        this.stompClient.send('/chat/' + this.state.currentChatroom, {}, message);
-        this.setState({message: ''});
-    }
-
-    setRoom = (e) => {
-        this.setState({chatroom: e.target.value});
-        console.log(this.state.chatroom);
+    sendMessage = () => {
+        console.log("DIS SHIT EMPTY: " + this.state.message.userId);
+        this.stompClient.send('/chat/' + this.state.currentChatroom, {}, JSON.stringify(this.state.message));
+        console.log("send to " + this.state.currentChatroom);
+        this.setState({message: {text: ''}});
     };
 
     setText = (e) => {
-        this.setState({message: e.target.value});
+        this.setState({message: {text: e.target.value, userId: this.state.user.id, userName: this.state.user.firstname + ' ' + this.state.user.lastname}});
     };
 
     render() {
-        console.log("chat content");
-        console.log(this.state.messages);
         return (
-            <div width="100%" className="columncontainer">
+            <div className="columncontainer">
                 <Header name={"Chat: " + this.state.currentChatroom}/>
-                <div className="rowcontainer chatheader">
-                    <div className="row">
-
-                        <div className="col s10 m10 l10 chatbar">
-                            <StyledTextField
-                                onChange={this.setRoom}
-                                label="Chatroom"
-                                value={this.state.chatroom}
-                            />
-                        </div>
-                        <div className="col s2 m2 l2 chatbutton">
-                            <button onClick={(e) => this.ChangeRoom(e)}>Change room</button>
-                        </div>
-                    </div>
-                </div>
                 <div className="rowcontainer chat">
                     <div className="groupschat">
-                    </div>
-                    <div className="paddingnator">
-                        {this.state.messages.map((msg, key) => {
-                                return (<div key={key} className="speech-bubble chatmargin" id={key}>{msg}</div>)
+                        <List>
+                            { this.state.groups && this.state.groups.length > 0 ?
+                                this.state.groups.map((group, index) => (
+                                    <ListItem key={index} primaryText={group.name}
+                                              onClick={() => this.ChangeRoom(group.name)}/>))
+                                : <ListItem key={1} primaryText="Geen groepen!"/>
                             }
-                        )}
+                        </List>
+                    </div>
+                    <div className="texting paddingnator">
+                        {
+                            this.state.messages.map((msg, key) => {
+                                    console.log("MESSAGE: " + msg.text);
+                                    console.log("IDDDDDD: " + msg.userId);
+                                    if (msg.userId === this.state.user.id) {
+                                        return (
+                                            <div key={key} className="speech-bubbleYou chatMarginYou" id={key}>{msg.userName}: {msg.text}</div>
+                                        )
+                                    } else {
+                                        return (
+                                            <div key={key} className="speech-bubble chatmargin" id={key}>{msg.userName}: {msg.text}</div>
+                                        )
+                                    }
+                                }
+                            )
+                        }
                     </div>
                 </div>
                 <div className="divider"></div>
                 <form>
-                <div className="rowcontainer messagetyper">
-                    <div className="chatbar">
-                        <StyledTextField
-                            onChange={this.setText}
-                            label="Message"
-                            value={this.state.message}
-                        />
+                    <div className="rowcontainer messagetyper">
+                        <div className="chatbar">
+                            <StyledTextField
+                                onChange={this.setText}
+                                label="Message"
+                                value={this.state.message.text}
+                            />
+                        </div>
+                        <div className="marginator">
+                            <a className="btnSize waves-effect deep-orange darken-4 waves-light btn"
+                               onClick={(e) => this.sendMessage(e)}>send</a>
+                        </div>
                     </div>
-                    <div className="marginator">
-                        <button onClick={(e) => this.sendMessage(e)}>send</button>
-                    </div>
-                </div>
                 </form>
 
             </div>
