fe273def4a76d955171c75f11be181ac2f1d792a
LorenzLambot <lorenz.lambot@gmail.com>
2018-03-22 00:29:13 +0100

    RefactorChordsheet + test

---
+++ src/__test__/MusicScore.test.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import Partituur from "../Components/MusicComponents/Partituur";
+import Adapter from 'enzyme-adapter-react-16'
+import {configure, shallow} from "enzyme";
+
+configure({ adapter: new Adapter() });
+
+describe('MusicScore', () => {
+
+    it('knows that 2 and 2 make 4', () => {
+        expect(2 + 2).toBe(4);
+    });
+
+    it('expects svg to be rendered', () => {
+        const musicScord = shallow(<Partituur
+            fileFormat="Canon.gp5"
+            content="/BackendJsonSimulated/Cano.gp5"
+        />);
+        const svg = musicScord.find('svg');
+        expect(svg).toBeDefined();
+    });
+});
